02d4376b1bef5555183a42cfd04ec224af4e9856
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-01 19:48:14 +0800

    updated the youtube sites

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,6 @@
 // contentScript.js
+
+//ENTER FUNCTION
 const textInputs = document.querySelectorAll('input[type="text"], textarea');
 
 textInputs.forEach(textInput => {
@@ -14,3 +16,43 @@ textInputs.forEach(textInput => {
     }
   });
 });
+
+
+
+// Function to capture input
+function captureInput(inputValue) {
+  chrome.storage.local.get({ inputs: [] }, result => {
+    const storedInputs = result.inputs;
+    storedInputs.push(inputValue);
+    chrome.storage.local.set({ inputs: storedInputs });
+  });
+}
+
+
+// Function to capture search input
+function captureSearchInput(inputValue) {
+  captureInput(inputValue);
+}
+
+const searchForm = document.querySelector('#search-form');
+const searchButton = document.querySelector('#search-icon-legacy');
+
+if (searchForm) {
+  searchForm.addEventListener('submit', function(event) {
+    const searchInput = searchForm.querySelector('input[type="text"]');
+    if (searchInput) {
+      const inputValue = searchInput.value;
+      captureSearchInput(inputValue);
+    }
+  });
+
+  if (searchButton) {
+    searchButton.addEventListener('click', function(event) {
+      const searchInput = searchForm.querySelector('input[type="text"]');
+      if (searchInput) {
+        const inputValue = searchInput.value;
+        captureSearchInput(inputValue);
+      }
+    });
+  }
+}
