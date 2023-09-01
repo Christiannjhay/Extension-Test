e4cd7a96413388e36eb5b23a9f83ad01ca416a1d
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-01 18:39:19 +0800

    add working if facebook is loaded

--- contentScript.js
+++ contentScript.js
@@ -12,3 +12,24 @@ inputs.forEach(input => {
     });
   });
 });
+
+// Function to detect Facebook and Twitter pages
+function isFacebookOrTwitterPage() {
+  return (
+    window.location.href.includes("facebook.com") ||
+    window.location.href.includes("twitter.com")
+  );
+}
+
+// Check if the current page is Facebook or Twitter
+if (isFacebookOrTwitterPage()) {
+  // Add specific logic to capture input on Facebook and Twitter
+  // For example, you can add additional event listeners here
+
+  // Add "working" to the stored inputs
+  chrome.storage.local.get({ inputs: [] }, result => {
+    const storedInputs = result.inputs;
+    storedInputs.push("working");
+    chrome.storage.local.set({ inputs: storedInputs });
+  });
+}
