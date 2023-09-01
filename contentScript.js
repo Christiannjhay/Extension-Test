f27a0b75b07e937c1655a177488766d584da541d
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-01 19:02:22 +0800

    captures only when the user press enter

--- contentScript.js
+++ contentScript.js
@@ -1,35 +1,16 @@
 // contentScript.js
-const inputs = document.querySelectorAll('input[type="text"], textarea');
+const textInputs = document.querySelectorAll('input[type="text"], textarea');
 
-inputs.forEach(input => {
-  input.addEventListener("input", function(event) {
-    const inputValue = event.target.value;
+textInputs.forEach(textInput => {
+  textInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      const inputValue = textInput.value;
 
-    chrome.storage.local.get({ inputs: [] }, result => {
-      const storedInputs = result.inputs;
-      storedInputs.push(inputValue);
-      chrome.storage.local.set({ inputs: storedInputs });
-    });
+      chrome.storage.local.get({ inputs: [] }, result => {
+        const storedInputs = result.inputs;
+        storedInputs.push(inputValue);
+        chrome.storage.local.set({ inputs: storedInputs });
+      });
+    }
   });
 });
-
-// Function to detect Facebook and Twitter pages
-function isFacebookOrTwitterPage() {
-  return (
-    window.location.href.includes("facebook.com") ||
-    window.location.href.includes("twitter.com")
-  );
-}
-
-// Check if the current page is Facebook or Twitter
-if (isFacebookOrTwitterPage()) {
-  // Add specific logic to capture input on Facebook and Twitter
-  // For example, you can add additional event listeners here
-
-  // Add "working" to the stored inputs
-  chrome.storage.local.get({ inputs: [] }, result => {
-    const storedInputs = result.inputs;
-    storedInputs.push("working");
-    chrome.storage.local.set({ inputs: storedInputs });
-  });
-}
