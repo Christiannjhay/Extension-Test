9a5026ab37f6ba49f2ea764277c1742cb45a4071
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-03 19:04:08 +0800

    updated daigler site

--- contentScript.js
+++ contentScript.js
@@ -1,25 +1,4 @@
-// contentScript.js
-
-//ENTER FUNCTION
-const textInputs = document.querySelectorAll('input[type="text"], textarea');
-
-textInputs.forEach(textInput => {
-  textInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      const inputValue = textInput.value;
-
-      chrome.storage.local.get({ inputs: [] }, result => {
-        const storedInputs = result.inputs;
-        storedInputs.push(inputValue);
-        chrome.storage.local.set({ inputs: storedInputs });
-      });
-    }
-  });
-});
-
-
-
-// Function to capture input
+// Function to capture input from text inputs
 function captureInput(inputValue) {
   chrome.storage.local.get({ inputs: [] }, result => {
     const storedInputs = result.inputs;
@@ -28,14 +7,59 @@ function captureInput(inputValue) {
   });
 }
 
-
 // Function to capture search input
 function captureSearchInput(inputValue) {
   captureInput(inputValue);
 }
 
+// Function to capture input from text areas with the specified class
+function captureTextAreaInput() {
+  const textAreas = document.querySelectorAll('.rui-textarea.form-control.w-100.mx-0.mb-2');
+  
+  textAreas.forEach(textArea => {
+    textArea.addEventListener("keydown", function(event) {
+      if (event.key === "Enter") {
+        const inputValue = textArea.value;
+        captureInput(inputValue);
+      }
+    });
+  });
+}
 
-//YOUTUBE FUNCTION
+// Function to capture input when the button is clicked
+function captureButtonInput() {
+  const button = document.querySelector('.btn.btn-primary.mt-1.col.mr-2');
+  
+  if (button) {
+    button.addEventListener("click", function() {
+      const textArea = document.querySelector('.rui-textarea.form-control.w-100.mx-0.mb-2');
+      if (textArea) {
+        const inputValue = textArea.value;
+        console.log("Textarea content: ", inputValue);
+        captureInput(inputValue);
+      }
+    });
+  }
+}
+
+// Call the function to capture input from text inputs
+const textInputs = document.querySelectorAll('input[type="text"], textarea');
+
+textInputs.forEach(textInput => {
+  textInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      const inputValue = textInput.value;
+      captureInput(inputValue);
+    }
+  });
+});
+
+// Call the function to capture input from text areas
+captureTextAreaInput();
+
+// Call the function to capture input when the button is clicked
+captureButtonInput();
+// Your existing YouTube function
 const searchForm = document.querySelector('#search-form');
 const searchButton = document.querySelector('#search-icon-legacy');
 
