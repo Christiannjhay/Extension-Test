3624add91c78969522ac847efc877b9e88c86431
Christiannjhay <cejysugarol@addu.edu.ph>
2023-09-05 20:09:05 +0800

    updated twitter  post function

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-// Function to capture input from text inputs
+// Store the inputs to local storage
 function captureInput(inputValue) {
   chrome.storage.local.get({ inputs: [] }, result => {
     const storedInputs = result.inputs;
@@ -12,10 +12,10 @@ function captureSearchInput(inputValue) {
   captureInput(inputValue);
 }
 
-// Function to capture input from text areas with the specified class
+// Daigler Enter function
 function captureTextAreaInput() {
   const textAreas = document.querySelectorAll('.rui-textarea.form-control.w-100.mx-0.mb-2');
-  
+
   textAreas.forEach(textArea => {
     textArea.addEventListener("keydown", function(event) {
       if (event.key === "Enter") {
@@ -26,10 +26,10 @@ function captureTextAreaInput() {
   });
 }
 
-// Function to capture input when the button is clicked
+// Daigler Send Button
 function captureButtonInput() {
   const button = document.querySelector('.btn.btn-primary.mt-1.col.mr-2');
-  
+
   if (button) {
     button.addEventListener("click", function() {
       const textArea = document.querySelector('.rui-textarea.form-control.w-100.mx-0.mb-2');
@@ -42,7 +42,20 @@ function captureButtonInput() {
   }
 }
 
-// Call the function to capture input from text inputs
+// Capture input from Twitter's "What's happening?" input field when the "Post" button is pressed
+document.body.addEventListener("click", function(event) {
+  const postButton = event.target.closest('[data-testid="tweetButtonInline"]');
+  if (postButton) {
+    const tweetInput = document.querySelector('[aria-label="Tweet text"]');
+    if (tweetInput) {
+      const inputValue = tweetInput.innerText;
+      captureInput(inputValue);
+    }
+  }
+});
+
+
+// All websites
 const textInputs = document.querySelectorAll('input[type="text"], textarea');
 
 textInputs.forEach(textInput => {
@@ -54,12 +67,7 @@ textInputs.forEach(textInput => {
   });
 });
 
-// Call the function to capture input from text areas
-captureTextAreaInput();
-
-// Call the function to capture input when the button is clicked
-captureButtonInput();
-// Your existing YouTube function
+// Youtube
 const searchForm = document.querySelector('#search-form');
 const searchButton = document.querySelector('#search-icon-legacy');
 
@@ -82,3 +90,16 @@ if (searchForm) {
     });
   }
 }
+
+// Capture input from elements with class "DraftEditor-root" when a specific button is pressed
+const targetButtonClass = 'css-1dbjc4n r-l5o3uw r-42olwf r-sdzlij r-1phboty r-rs99b7 r-19u6a5r r-2yi16 r-1qi8awa r-icoktb r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr';
+
+document.body.addEventListener("click", function(event) {
+  if (event.target.classList.contains(targetButtonClass)) {
+    const draftEditor = document.querySelector('.DraftEditor-root');
+    if (draftEditor) {
+      const inputValue = draftEditor.innerText;
+      captureInput(inputValue);
+    }
+  }
+});
